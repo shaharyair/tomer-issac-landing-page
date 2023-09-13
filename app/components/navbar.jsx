402bbbe673e1e983b435878f5101f5c7f9ae7692
Shahar Yair <shaharyair12@gmail.com>
2023-09-13 21:13:51 +0300

    feat(temp): add navbar

---
+++ app/components/navbar.jsx
@@ -0,0 +1,184 @@
+"use client";
+
+import { useState, useEffect, useRef } from "react";
+import Link from "next/link";
+
+import { Fade as Hamburger } from "hamburger-react";
+import {
+  FaInstagram,
+  FaYoutube,
+  FaFacebookF,
+  FaPhone,
+  FaTiktok,
+  FaWhatsapp,
+} from "react-icons/fa6";
+
+function Navbar() {
+  const [mobileNavbar, setMobileNavbar] = useState(false);
+  const navbarRef = useRef(null);
+
+  const handleToggle = () => {
+    setMobileNavbar(!mobileNavbar);
+  };
+
+  useEffect(() => {
+    const closeMobileNavbarOnResize = () => {
+      if (window.innerWidth >= 1024) {
+        setMobileNavbar(false);
+      }
+    };
+
+    const handleLinkClick = (event) => {
+      // Check if the click target is a link or a descendant of a link
+      const isLinkClicked = event.target.closest("a");
+
+      // If a link was clicked, close the mobile navbar
+      if (isLinkClicked) {
+        setMobileNavbar(false);
+      }
+    };
+
+    const handleOutsideClick = (event) => {
+      // Check if the click target is outside the navbar container
+      if (navbarRef.current && !navbarRef.current.contains(event.target)) {
+        setMobileNavbar(false);
+      }
+    };
+
+    document.addEventListener("click", handleLinkClick);
+    document.addEventListener("click", handleOutsideClick);
+    window.addEventListener("resize", closeMobileNavbarOnResize);
+
+    return () => {
+      document.removeEventListener("click", handleLinkClick);
+      document.removeEventListener("click", handleOutsideClick);
+      window.removeEventListener("resize", closeMobileNavbarOnResize);
+    };
+  }, [mobileNavbar]);
+
+  return (
+    <>
+      <nav className="fixed left-0 top-0 z-50 flex h-navbarHeight w-full items-center justify-center bg-navbarBlack/75 drop-shadow-lg backdrop-blur-sm">
+        <div
+          ref={navbarRef}
+          className="container flex items-center justify-between px-5 text-white"
+        >
+          <ComponentLinks mobileNavbar={mobileNavbar} />
+          <Link
+            href="#home"
+            className="text-main p-2 text-3xl lg:order-3 lg:p-0"
+          >
+            <h1 className="font-extralight tracking-widest">TOMER</h1>
+          </Link>
+          <SocialLinks />
+          <HamburgerButton
+            mobileNavbar={mobileNavbar}
+            handleToggle={handleToggle}
+          />
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
+
+function SocialLinks() {
+  return (
+    <>
+      <div className=" flex items-center justify-center gap-2 text-xl text-white lg:gap-3">
+        <Link href="https://www.instagram.com/cashiyado/">
+          <div className="rounded-full bg-black p-2 transition-colors duration-300 hover:bg-white hover:text-black">
+            <FaInstagram />
+          </div>
+        </Link>
+        <Link href="https://www.facebook.com/profile.php?id=100013457760835&mibextid=nW3QTL">
+          <div className="rounded-full bg-black p-2 transition-colors duration-300 hover:bg-white hover:text-black">
+            <FaFacebookF />
+          </div>
+        </Link>
+        <Link href="https://youtube.com/@Cashiyado?si=rbU4LP1UypUNhEvw">
+          <div className="rounded-full bg-black p-2 transition-colors duration-300 hover:bg-white hover:text-black">
+            <FaYoutube />
+          </div>
+        </Link>
+        <Link href="https://www.tiktok.com/@cashiyado?_t=8fexZHKpoMT&_r=1">
+          <div className="rounded-full bg-black p-2 transition-colors duration-300 hover:bg-white hover:text-black">
+            <FaTiktok />
+          </div>
+        </Link>
+        <Link href="">
+          <div className="rounded-full bg-black p-2 transition-colors duration-300 hover:bg-white hover:text-black">
+            <FaWhatsapp />
+          </div>
+        </Link>
+      </div>
+    </>
+  );
+}
+
+function ComponentLinks({ mobileNavbar }) {
+  return (
+    <>
+      <div
+        className={` ${
+          mobileNavbar && "right-0"
+        } absolute -right-3/4 top-navbarHeight flex h-screen w-3/4 flex-col items-center justify-start divide-y divide-white/20 bg-navbarBlack/50 pt-2 text-center text-lg font-extralight text-white backdrop-blur-sm transition-mobileNavbar duration-150 lg:static lg:right-auto lg:top-auto lg:order-2 lg:mr-5 lg:h-auto lg:w-auto lg:flex-row lg:justify-center lg:gap-3 lg:divide-y-0 lg:bg-transparent lg:p-0 lg:backdrop-blur-none lg:transition-none`}
+      >
+        <Link
+          href="#home"
+          className="hover:text-main w-[95%] p-5 transition-colors  duration-300 lg:rounded-sm lg:px-4 lg:py-2"
+        >
+          ראשי
+        </Link>
+        <Link
+          href="#home"
+          className="hover:text-main w-[95%] p-5 transition-colors  duration-300 lg:rounded-sm lg:px-4 lg:py-2"
+        >
+          ראשי
+        </Link>
+        <Link
+          href="#home"
+          className="hover:text-main w-[95%] p-5 transition-colors  duration-300 lg:rounded-sm lg:px-4 lg:py-2 "
+        >
+          ראשי
+        </Link>
+        <Link
+          href="#home"
+          className="hover:text-main w-[95%] p-5 transition-colors  duration-300 lg:rounded-sm lg:px-4 lg:py-2 "
+        >
+          ראשי
+        </Link>
+        <Link
+          href="#home"
+          className="hover:text-main w-[95%] p-5 transition-colors  duration-300 lg:rounded-sm lg:px-4 lg:py-2 "
+        >
+          ראשי
+        </Link>
+        <Link
+          href="#home"
+          className="hover:text-main w-[95%] p-5 transition-colors  duration-300 lg:rounded-sm lg:px-4 lg:py-2 "
+        >
+          ראשי
+        </Link>
+      </div>
+    </>
+  );
+}
+
+function HamburgerButton({ mobileNavbar, handleToggle }) {
+  return (
+    <>
+      <div className="block lg:hidden">
+        <Hamburger
+          toggled={mobileNavbar}
+          toggle={handleToggle}
+          size={20}
+          direction="right"
+          duration={0.15}
+          rounded
+        />
+      </div>
+    </>
+  );
+}
