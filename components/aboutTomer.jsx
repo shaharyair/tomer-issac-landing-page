be6e67a6e13a8815d320d7d5dd9e5364b6d119be
Shahar Yair <shaharyair12@gmail.com>
2023-09-14 17:57:03 +0300

    fix: layout aboutTomer

--- components/aboutTomer.jsx
+++ components/aboutTomer.jsx
@@ -4,40 +4,39 @@ import Hero1 from "public/assests/heroCarousel/1.JPG";
 function AboutTomer() {
   return (
     <>
-      <div className="relative my-5 h-[500px] w-full">
+      <div className="relative mt-5 flex h-[500px] w-full items-center justify-center">
         <Image
           src={Hero1}
           alt="/"
           layout="fill"
           objectFit="cover"
           objectPosition="center"
-          className=" blur-xs brightness-50"
+          className=" absolute left-0 top-0 -z-10 brightness-50"
         />
-        <div className="absolute left-1/2 top-1/2 -translate-x-2/4 -translate-y-2/4">
-          <div
-            className="flex items-center justify-center"
-            style={{ direction: "rtl" }}
-          >
-            <div className="flex flex-col items-start justify-center gap-10 text-white">
-              <h1 className="text-4xl font-semibold drop-shadow-sm">
-                קצת עליי...
-              </h1>
-              <p className=" font-normal">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam
-                illo officia, hic deleniti, accusantium officiis temporibus
-                reiciendis perspiciatis error, odio esse ut corporis ipsa eum
-                laboriosam praesentium iusto recusandae quibusdam consequuntur
-                ullam totam unde incidunt tempore! Quaerat totam delectus minus
-                quisquam optio blanditiis numquam perferendis alias et, eaque
-                maiores, ratione voluptate aliquid velit. Necessitatibus autem
-                quod consequatur atque ab repellendus nesciunt eaque expedita
-                animi aut error et, velit explicabo repudiandae magnam placeat
-                accusamus odit. Ratione quo omnis repellat, sit fugit dicta
-                maxime impedit, sequi facere obcaecati est molestias nulla
-                similique sint exercitationem vero provident, quibusdam
-                cupiditate aut. Asperiores, modi nulla.
-              </p>
-            </div>
+        <div
+          className="container flex items-center justify-center p-5"
+          style={{ direction: "rtl" }}
+        >
+          <div className="flex flex-col items-start justify-center gap-2 text-white">
+            <h2 className="text-3xl font-bold drop-shadow-md">קצת עליי...</h2>
+            <h1 className="text-4xl font-bold text-main drop-shadow-md">
+              Cashiyado
+            </h1>
+            <p className="mt-4">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam
+              illo officia, hic deleniti, accusantium officiis temporibus
+              reiciendis perspiciatis error, odio esse ut corporis ipsa eum
+              laboriosam praesentium iusto recusandae quibusdam consequuntur
+              ullam totam unde incidunt tempore! Quaerat totam delectus minus
+              quisquam optio blanditiis numquam perferendis alias et, eaque
+              maiores, ratione voluptate aliquid velit. Necessitatibus autem
+              quod consequatur atque ab repellendus nesciunt eaque expedita
+              animi aut error et, velit explicabo repudiandae magnam placeat
+              accusamus odit. Ratione quo omnis repellat, sit fugit dicta maxime
+              impedit, sequi facere obcaecati est molestias nulla similique sint
+              exercitationem vero provident, quibusdam cupiditate aut.
+              Asperiores, modi nulla.
+            </p>
           </div>
         </div>
       </div>
