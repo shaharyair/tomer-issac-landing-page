51036078550acd2027b5cccc51375f770cac7d1c
Shahar Yair <shaharyair12@gmail.com>
2023-09-21 18:06:38 +0300

    showcaseCarousel: Add stillImagesCarousel

--- components/showcaseCarousel.jsx
+++ components/showcaseCarousel.jsx
@@ -1,18 +1,18 @@
 "use client";
 
 import EmblaCarousel from "./carousel";
-import { marketingGuidanceVideosUrl } from "@/config";
+import { marketingGuidanceVideosUrl, stillImages } from "@/config";
 
 export default function ShowcaseCarousel() {
   return (
     <>
       <MarketingGuidanceVideoCarousel />
+      <StillImagesCarousel />
     </>
   );
 }
 
 function MarketingGuidanceVideoCarousel() {
-  // Corrected component name here
   return (
     <div
       className="flex h-full w-full flex-col items-center justify-center pt-5"
@@ -33,6 +33,33 @@ function MarketingGuidanceVideoCarousel() {
         }}
         slides={marketingGuidanceVideosUrl}
         mediaType="video"
+      />
+    </div>
+  );
+}
+
+function StillImagesCarousel() {
+  return (
+    <div
+      className="flex h-full w-full flex-col items-center justify-center pt-5"
+      id="still-images"
+    >
+      <h1 className="text-4xl font-thin text-white drop-shadow-md">
+        תמונות סטילס
+      </h1>
+      <EmblaCarousel
+        slideStyle={
+          "w-[90vw] lg:w-[35vw] h-[45vh] pt-5 mx-2 drop drop-shadow-sm object-cover object-center"
+        }
+        containerStyle={"overflow-hidden"}
+        options={{
+          loop: true,
+          align: "center",
+          skipSnaps: true,
+          duration: 50,
+          slidesToScroll: 1,
+        }}
+        slides={stillImages}
       />
     </div>
   );
