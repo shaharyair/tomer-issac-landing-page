cb889b14daeb93964bda4fdbe7e11c9a7c6b7c0d
Shahar Yair <shaharyair12@gmail.com>
2023-09-21 23:55:33 +0300

    aboutTomer: Update styling
    Add tailwind config, drop shadow stroke prop

--- components/aboutTomer.jsx
+++ components/aboutTomer.jsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
-import Hero1 from "public/assests/heroCarousel/1.JPG";
+import { stillImages } from "@/config";
 
 function AboutTomer() {
   return (
     <>
-      <div className="relative mt-5 flex h-[500px] w-full items-center justify-center">
+      <div className="relative mt-5 flex h-[60vh] max-h-[500px] w-full items-center justify-center">
         <Image
-          src={Hero1}
+          src={stillImages.at(9)}
           alt="/"
           layout="fill"
           objectFit="cover"
@@ -14,28 +14,23 @@ function AboutTomer() {
           className=" absolute left-0 top-0 -z-10 brightness-50"
         />
         <div
-          className="container flex items-center justify-center p-5"
+          className="container flex max-w-5xl items-center justify-center p-10"
           style={{ direction: "rtl" }}
         >
           <div className="flex flex-col items-start justify-center gap-2 text-white">
-            <h2 className="text-3xl font-bold drop-shadow-md">קצת עליי...</h2>
-            <h1 className="text-4xl font-bold text-main drop-shadow-md">
-              Cashiyado
+            <h2 className="drop-shadow-stroke text-3xl font-bold">
+              קצת עליי...
+            </h2>
+            <h1 className="drop-shadow-stroke text-4xl font-semibold text-main">
+              Tomer Aizik
             </h1>
-            <p className="mt-4">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam
-              illo officia, hic deleniti, accusantium officiis temporibus
-              reiciendis perspiciatis error, odio esse ut corporis ipsa eum
-              laboriosam praesentium iusto recusandae quibusdam consequuntur
-              ullam totam unde incidunt tempore! Quaerat totam delectus minus
-              quisquam optio blanditiis numquam perferendis alias et, eaque
-              maiores, ratione voluptate aliquid velit. Necessitatibus autem
-              quod consequatur atque ab repellendus nesciunt eaque expedita
-              animi aut error et, velit explicabo repudiandae magnam placeat
-              accusamus odit. Ratione quo omnis repellat, sit fugit dicta maxime
-              impedit, sequi facere obcaecati est molestias nulla similique sint
-              exercitationem vero provident, quibusdam cupiditate aut.
-              Asperiores, modi nulla.
+            <p className="drop-shadow-stroke mt-4 text-lg">
+              בתחום התקשורת והמדיה אני עוסק מספר שנים, כיום גם כסטודנט. דרכי
+              התחילה בזמן התיכון במגמת תקשורת והתמקצעה במהלך השנים, החל מהשירות
+              הצבאי כצלם ועורך אותו סיימתי עם ארבע תעודות הצטיינות, כשברור שזה
+              הקו בו אמשיך לעסוק מעתה והילך. בהמשך, זכיתי לפגוש ולעבוד עם חברות
+              ואנשים רבים לאורך השנים על מגוון סרטונים ויצירות שאת חלקם ניתן
+              לראות באתר. מחכה לעבוד גם איתכם!
             </p>
           </div>
         </div>
