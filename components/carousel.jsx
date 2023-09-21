d31fef987e4028a372c77b164db5e3786092235c
Shahar Yair <shaharyair12@gmail.com>
2023-09-21 14:38:24 +0300

    heroCarousel: Change images to urls from config.js
    Add width and height props to Image

--- components/carousel.jsx
+++ components/carousel.jsx
@@ -1,22 +1,27 @@
-'use client'
+"use client";
 
 import Image from "next/image";
 import useEmblaCarousel from "embla-carousel-react";
 
 const EmblaCarousel = (props) => {
-  const { options, slides, plugins, slideStyle, containerStyle } = props
+  const { options, slides, plugins, slideStyle, containerStyle } = props;
   const [emblaRef] = useEmblaCarousel(options, plugins);
 
   return (
-    <div className='embla'>
+    <div className="embla">
       <div className={containerStyle} ref={emblaRef}>
-        <div className='flex'>
+        <div className="flex">
           {slides.map((img, index) => (
-            <div className='flex-shrink-0 flex-grow-0 flex-auto relative' key={index}>
+            <div
+              className="relative flex-auto flex-shrink-0 flex-grow-0"
+              key={index}
+            >
               <Image
                 className={slideStyle}
                 src={img}
-                alt='/'
+                alt="/"
+                width={1920}
+                height={1080}
               />
             </div>
           ))}
@@ -26,5 +31,4 @@ const EmblaCarousel = (props) => {
   );
 };
 
-export default EmblaCarousel
-
+export default EmblaCarousel;
--- components/heroCarousel.jsx
+++ components/heroCarousel.jsx
@@ -1,25 +1,26 @@
-'use client'
+"use client";
 
-import EmblaCarousel from './carousel'
-import Autoplay from 'embla-carousel-autoplay'
+import EmblaCarousel from "./carousel";
+import Autoplay from "embla-carousel-autoplay";
 
-import Hero1 from 'public/assests/heroCarousel/1.JPG'
-import Hero2 from 'public/assests/heroCarousel/2.JPG'
-import Hero3 from 'public/assests/heroCarousel/3.JPG'
-
+import { heroCarouselImages } from "@/config";
 
 export default function HeroCarousel() {
-  const slides = [Hero1, Hero2, Hero3]
-
   const autoplayOptions = {
     delay: 7500,
     stopOnInteraction: false,
     rootNode: (emblaRoot) => emblaRoot.parentElement,
-  }
+  };
 
   return (
     <>
-      <EmblaCarousel slideStyle={'w-screen h-[80vh] object-cover object-center'} containerStyle={'overflow-hidden'} options={{loop: true, align: 'center', skipSnaps: true, duration: 50}} slides={slides} plugins={[Autoplay(autoplayOptions)]}  / >   
+      <EmblaCarousel
+        slideStyle={"w-screen h-[80vh] object-cover object-center"}
+        containerStyle={"overflow-hidden"}
+        options={{ loop: true, align: "center", skipSnaps: true, duration: 50 }}
+        slides={heroCarouselImages}
+        plugins={[Autoplay(autoplayOptions)]}
+      />
     </>
   );
 }
