2946d084e88714a3c7565931520fb0daf52c2e52
Shahar Yair <shaharyair12@gmail.com>
2023-09-21 22:57:27 +0300

    showcaseCarousel: Add musicalClipsCarousel, PodcastsEditsCarousel

--- components/showcaseCarousel.jsx
+++ components/showcaseCarousel.jsx
@@ -4,6 +4,8 @@ import EmblaCarousel from "./carousel";
 import {
   marketingGuidanceVideosUrl,
   mashupVideos,
+  musicalClips,
+  podcastsEditsVideos,
   stillImages,
 } from "@/config";
 
@@ -12,6 +14,8 @@ export default function ShowcaseCarousel() {
     <>
       <MarketingGuidanceVideoCarousel />
       <StillImagesCarousel />
+      <MusicalClipsCarousel />
+      <PodcastsEditsCarousel />
       <MashupVideosCarousel />
     </>
   );
@@ -92,6 +96,62 @@ function MashupVideosCarousel() {
           slidesToScroll: 1,
         }}
         slides={mashupVideos}
+        mediaType="video"
+      />
+    </div>
+  );
+}
+
+function MusicalClipsCarousel() {
+  return (
+    <div
+      className="mt-5 flex h-full w-full flex-col items-center justify-center gap-5"
+      id="musical-clips"
+    >
+      <h1 className="text-4xl font-thin text-white drop-shadow-md">
+        קליפים מוזיקליים
+      </h1>
+      <EmblaCarousel
+        slideStyle={
+          "w-[85vw] lg:w-[35vw] h-[45vh] pt-5 mx-2 drop drop-shadow-sm bg-black/20"
+        }
+        containerStyle={"overflow-hidden"}
+        options={{
+          loop: true,
+          align: "center",
+          skipSnaps: true,
+          duration: 50,
+          slidesToScroll: 1,
+        }}
+        slides={musicalClips}
+        mediaType="video"
+      />
+    </div>
+  );
+}
+
+function PodcastsEditsCarousel() {
+  return (
+    <div
+      className="mt-5 flex h-full w-full flex-col items-center justify-center gap-5"
+      id="podcasts-edits"
+    >
+      <h1 className="text-4xl font-thin text-white drop-shadow-md">
+        עריכת פודקאסטים
+      </h1>
+      <EmblaCarousel
+        slideStyle={
+          "w-[85vw] lg:w-[35vw] h-[45vh] pt-5 mx-2 drop drop-shadow-sm bg-black/20"
+        }
+        containerStyle={"overflow-hidden"}
+        options={{
+          loop: true,
+          align: "center",
+          skipSnaps: true,
+          duration: 50,
+          slidesToScroll: 1,
+        }}
+        slides={podcastsEditsVideos}
         mediaType="video"
       />
     </div>
