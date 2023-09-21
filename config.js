d9959791a9f2768a3da03402ee2db1e1951e2e2c
Shahar Yair <shaharyair12@gmail.com>
2023-09-21 17:39:17 +0300

    config: Add links
    guidanceMarketing videos and still images

--- config.js
+++ config.js
@@ -15,4 +15,64 @@ const heroCarouselImages = [
   ),
 ];
 
-export { heroCarouselImages };
+const marketingGuidanceVideosUrl = [
+  getDirectDriveLink(
+    "https://drive.google.com/file/d/1ec_0GgWQo_o2RRLp-_ogxRa2yyY3dOaO/view?usp=sharing", // חוג חינם
+  ),
+  getDirectDriveLink(
+    "https://drive.google.com/file/d/1taLYf1tdr5FVXLoAX0OzVf7JzimT2f40/view?usp=sharing", // 60 שנה
+  ),
+  getDirectDriveLink(
+    "https://drive.google.com/file/d/1ry_EWZNioilcZIXpcHcWPHuGqodjZHms/view?usp=sharing", // בת ים פילאטיס
+  ),
+  getDirectDriveLink(
+    "https://drive.google.com/file/d/160aQStdE7cao8WRrrgVxQYUxFzEyElDX/view?usp=sharing", // ליהי
+  ),
+  getDirectDriveLink(
+    "https://drive.google.com/file/d/17-PA7xxjwe0DAVkj4-XrhHQR1MG72SwC/view?usp=sharing", // סרטון ביטחות חשמל
+  ),
+  getDirectDriveLink(
+    "https://drive.google.com/file/d/1LTPzQXKhfkmyG7dR-nqyJ5wCzdqPr3dR/view?usp=sharing", // תצוגה רגילה לרשתות
+  ),
+];
+
+const stillImages = [
+  getDirectDriveLink(
+    "https://drive.google.com/file/d/1HSBOVq0utPy9XNgdQNJwQXjIX81RZfc8/view?usp=sharing",
+  ),
+  getDirectDriveLink(
+    "https://drive.google.com/file/d/1HYd-zF3k3GirTRKAAjStJMtOU2ca-iyb/view?usp=sharing",
+  ),
+  getDirectDriveLink(
+    "https://drive.google.com/file/d/1HOQZiWhxAeTqsxNhv8ND9w66Lqi_zFH5/view?usp=sharing",
+  ),
+  getDirectDriveLink(
+    "https://drive.google.com/file/d/1HUQ7d_9XnF4pdEwVuAhMAw9TAivfrTxY/view?usp=sharing",
+  ),
+  getDirectDriveLink(
+    "https://drive.google.com/file/d/1HLeisF-CR9EyM1ZzK9lRUtQQUY7VIP1K/view?usp=sharing",
+  ),
+  getDirectDriveLink(
+    "https://drive.google.com/file/d/1HUuHLEADBmw--43lzbHIdqVeJB6bim4o/view?usp=sharing",
+  ),
+  getDirectDriveLink(
+    "https://drive.google.com/file/d/1HKP7dYDdDO0mSNs_5Yf2SY5NR5mRZzIW/view?usp=sharing",
+  ),
+  getDirectDriveLink(
+    "https://drive.google.com/file/d/1UwqsDPMFvT8ZCBbCOP1Jr3MEsZ1PqXtn/view?usp=sharing",
+  ),
+  getDirectDriveLink(
+    "https://drive.google.com/file/d/1APW948OlYo3Hi5_pVYBFyzJPcxjuYp2c/view?usp=sharing",
+  ),
+  getDirectDriveLink(
+    "https://drive.google.com/file/d/17mm0Bl1pDtJYGKymlXndb3jtT80zxpsM/view?usp=sharing",
+  ),
+  getDirectDriveLink(
+    "https://drive.google.com/file/d/1hzWcWvw4eU_tdCr3OVaLnIZFrgV26OVk/view?usp=sharing",
+  ),
+  getDirectDriveLink(
+    "https://drive.google.com/file/d/18bu7B2-JTkD5Nr4qU-SZHH8ViSjvTth2/view?usp=sharing",
+  ),
+];
+
+export { heroCarouselImages, marketingGuidanceVideosUrl, stillImages };
