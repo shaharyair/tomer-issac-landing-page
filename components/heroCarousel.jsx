a895d2165a0a1eedb953d49d720c3aa687722c0f
Shahar Yair <shaharyair12@gmail.com>
2023-09-21 15:59:43 +0300

    hero: Add header and contact links

--- components/heroCarousel.jsx
+++ components/heroCarousel.jsx
@@ -3,7 +3,12 @@
 import EmblaCarousel from "./carousel";
 import Autoplay from "embla-carousel-autoplay";
 
+import Button from "./button";
+import Link from "next/link";
+
 import { heroCarouselImages } from "@/config";
+
+import { FaPhone, FaWhatsapp } from "react-icons/fa6";
 
 export default function HeroCarousel() {
   const autoplayOptions = {
@@ -14,13 +19,51 @@ export default function HeroCarousel() {
 
   return (
     <>
-      <EmblaCarousel
-        slideStyle={"w-screen h-[80vh] object-cover object-center"}
-        containerStyle={"overflow-hidden"}
-        options={{ loop: true, align: "center", skipSnaps: true, duration: 50 }}
-        slides={heroCarouselImages}
-        plugins={[Autoplay(autoplayOptions)]}
-      />
+      <div className="relative">
+        <EmblaCarousel
+          slideStyle={
+            "w-screen h-[80vh] object-cover object-center brightness-50"
+          }
+          containerStyle={"overflow-hidden"}
+          options={{
+            loop: true,
+            align: "center",
+            skipSnaps: true,
+            duration: 50,
+          }}
+          slides={heroCarouselImages}
+          plugins={[Autoplay(autoplayOptions)]}
+        />
+        <div
+          className=" absolute left-1/2 top-[55%] flex -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center gap-16 text-center lg:gap-20"
+          style={{ direction: "rtl" }}
+        >
+          <h1 className=" whitespace-nowrap text-5xl font-thin tracking-widest text-white opacity-75 drop-shadow-md lg:text-8xl">
+            TOMER AIZIK
+          </h1>
+          <p className="text-lg font-thin text-white opacity-75 drop-shadow-md lg:text-2xl">
+            לפרטים נוספים:
+          </p>
+          <div className="flex items-center justify-center gap-5 lg:gap-10">
+            <Button className="text-white">
+              <a
+                href="https://api.whatsapp.com/send?phone=972507677702"
+                className="flex items-center justify-center gap-2 whitespace-nowrap"
+              >
+                Whatsapp <FaWhatsapp className="text-xl" />
+              </a>
+            </Button>
+            <Button className="p-10 text-white">
+              <a
+                href="tel:+972507677702"
+                className="flex items-center justify-center gap-2 whitespace-nowrap"
+              >
+                התקשרו אליי <FaPhone className="text-xl" />
+              </a>
+            </Button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- config.js
+++ config.js
@@ -2,10 +2,10 @@ import getDirectDriveLink from "./utils/getDirectDriveLink";
 
 const heroCarouselImages = [
   getDirectDriveLink(
-    "https://drive.google.com/file/d/1AvC-Fqu6wq-Mghzz4qYDG0mE0W7nr2v_/view?usp=sharing",
+    "https://drive.google.com/file/d/1APW948OlYo3Hi5_pVYBFyzJPcxjuYp2c/view?usp=sharing",
   ),
   getDirectDriveLink(
-    "https://drive.google.com/file/d/1APW948OlYo3Hi5_pVYBFyzJPcxjuYp2c/view?usp=sharing",
+    "https://drive.google.com/file/d/1AvC-Fqu6wq-Mghzz4qYDG0mE0W7nr2v_/view?usp=sharing",
   ),
   getDirectDriveLink(
     "https://drive.google.com/file/d/1EqASrFzR7fRHEo6-NGSIve3M3j3xJ1kv/view?usp=sharing",
