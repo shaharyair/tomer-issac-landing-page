bc1a2a1ae6f04fbb135ad3da8c38f368b3674146
Shahar Yair <shaharyair12@gmail.com>
2023-09-22 01:23:48 +0300

    footer: improved style

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,18 +1,21 @@
-import Link from "next/link";
-import React from "react";
-
 function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <div className="flex h-12 w-full items-center justify-center bg-navbarBlack font-thin">
-      <h1 style={{ direction: "rtl" }} className="text-white">
-        נבנה על ידי{" "}
-        <Link href="https://api.whatsapp.com/send?phone=972586520441">
+    <div
+      className="flex h-12 w-full items-center justify-center gap-3 bg-navbarBlack font-thin text-white"
+      style={{ direction: "rtl" }}
+    >
+      <h1>נבנה על ידי</h1>
+      <h1>
+        <a
+          className="text-main brightness-125"
+          href="https://api.whatsapp.com/send?phone=972586520441"
+        >
           <u>Shahar Yair</u>
-        </Link>{" "}
-        &copy; {currentYear}
+        </a>
       </h1>
+      <h1>{currentYear} &copy; </h1>
     </div>
   );
 }
