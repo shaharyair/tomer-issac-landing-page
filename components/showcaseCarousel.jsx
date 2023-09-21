f418f2edf4a1261df488a8707d11b5ab67b5eed2
Shahar Yair <shaharyair12@gmail.com>
2023-09-21 17:40:05 +0300

    showcaseCarousel: Created marketing guidance carousel
    bug: not all videos stream because of large size

--- components/showcaseCarousel.jsx
+++ components/showcaseCarousel.jsx
@@ -1,21 +1,39 @@
-'use client'
+"use client";
 
-import EmblaCarousel from './carousel'
-
-import Hero1 from 'public/assests/heroCarousel/1.JPG'
-import Hero2 from 'public/assests/heroCarousel/2.JPG'
-import Hero3 from 'public/assests/heroCarousel/3.JPG'
-
+import EmblaCarousel from "./carousel";
+import { marketingGuidanceVideosUrl } from "@/config";
 
 export default function ShowcaseCarousel() {
-  const slides = [Hero1, Hero2, Hero3, Hero1, Hero2, Hero3]
-
   return (
     <>
-        <div className='w-full h-full flex flex-col justify-center items-center pt-5'>
-                <h1 className='text-4xl text-white font-thin shadow-md'>קטגוריה ראשונה</h1>
-            <EmblaCarousel slideStyle={'w-[90vw] lg:w-[30vw] object-cover object-center pt-5 mx-2 drop drop-shadow-sm'} containerStyle={'overflow-hidden w-full'} options={{loop: true, align: 'center', skipSnaps: true, duration: 50, slidesToScroll: 1}} slides={slides} / >   
-        </div>
+      <MarketingGuidanceVideoCarousel />
     </>
+  );
+}
+
+function MarketingGuidanceVideoCarousel() {
+  // Corrected component name here
+  return (
+    <div
+      className="flex h-full w-full flex-col items-center justify-center pt-5"
+      id="marketing-guidance-videos"
+    >
+      <h1 className="text-4xl font-thin text-white drop-shadow-md">
+        סרטי שיווק והדרכה
+      </h1>
+      <EmblaCarousel
+        slideStyle={"w-[80vw] lg:w-[35vw] pt-5 mx-2 drop drop-shadow-sm"}
+        containerStyle={"overflow-hidden"}
+        options={{
+          loop: true,
+          align: "center",
+          skipSnaps: true,
+          duration: 50,
+          slidesToScroll: 1,
+        }}
+        slides={marketingGuidanceVideosUrl}
+        mediaType="video"
+      />
+    </div>
   );
 }
