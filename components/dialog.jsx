9814fe045825860949de593000378ca6c4af7b7d
Shahar Yair <shaharyair12@gmail.com>
2023-09-22 01:02:05 +0300

    dialog: Improved style
    Add fadein animation, text direction rtl

--- components/dialog.jsx
+++ components/dialog.jsx
@@ -2,7 +2,10 @@ import Button from "./button";
 
 function Dialog({ message, onClick }) {
   return (
-    <div className="fixed left-0 top-0 z-[999] flex h-screen w-screen items-center justify-center bg-navbarBlack/50 text-center backdrop-blur-[5px]">
+    <div
+      className="fixed left-0 top-0 z-[999] flex h-screen w-screen animate-fade-in items-center justify-center bg-navbarBlack/50 text-center backdrop-blur-[5px]"
+      style={{ direction: "rtl" }}
+    >
       <div className=" flex h-[25vh] max-h-[200px] min-h-[150px] w-[75vw] min-w-[250px] max-w-[350px] flex-col items-center justify-evenly rounded-lg border border-white bg-navbarBlack p-5 drop-shadow-md">
         <p className=" text-lg font-thin text-white lg:text-xl">{message}</p>
         <Button onClick={onClick}>Close</Button>
