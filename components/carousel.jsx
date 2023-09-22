4b0df58e4bac7a43179392d23fb6c525251e01ae
Shahar Yair <shaharyair12@gmail.com>
2023-09-22 20:16:30 +0300

    carousel: Add lazy loading to videos

--- components/carousel.jsx
+++ components/carousel.jsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import useEmblaCarousel from "embla-carousel-react";
+import { useInView } from "react-intersection-observer";
 
 const EmblaCarousel = (props) => {
   const {
@@ -13,38 +14,48 @@ const EmblaCarousel = (props) => {
     mediaType,
     thumbnails,
   } = props;
+
   const [emblaRef] = useEmblaCarousel(options, plugins);
+
+  const [videoRef] = useInView({
+    threshold: 0.5,
+    rootMargin: "-50px 0px",
+    triggerOnce: true,
+  });
 
   return (
     <div className="embla">
       <div className={containerStyle} ref={emblaRef}>
         <div className="flex">
-          {slides.map((item, index) => (
-            <div
-              className="relative flex-auto flex-shrink-0 flex-grow-0"
-              key={index}
-            >
-              {mediaType === "video" ? (
-                <video
-                  className={slideStyle}
-                  src={item}
-                  controls
-                  type="video/mp4"
-                  preload="true"
-                  poster={thumbnails ? thumbnails[index] : null}
-                />
-              ) : (
-                <Image
-                  className={slideStyle}
-                  src={item}
-                  alt="/"
-                  width={1920}
-                  height={1080}
-                  loading="lazy"
-                />
-              )}
-            </div>
-          ))}
+          {slides.map((item, index) => {
+            return (
+              <div
+                className="relative flex-auto flex-shrink-0 flex-grow-0"
+                key={index}
+              >
+                {mediaType === "video" ? (
+                  <video
+                    className={slideStyle}
+                    src={item}
+                    type="video/mp4"
+                    preload="auto"
+                    controls
+                    poster={thumbnails ? thumbnails[index] : null}
+                    ref={videoRef}
+                  />
+                ) : (
+                  <Image
+                    className={slideStyle}
+                    src={item}
+                    alt="/"
+                    width={1920}
+                    height={1080}
+                    loading="lazy"
+                  />
+                )}
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
