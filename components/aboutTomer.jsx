a76c41e459df9d398a79d53552abf9f4cecf4a6d
Shahar Yair <shaharyair12@gmail.com>
2023-09-22 11:59:37 +0300

    Fix: Remove legacy props

--- components/aboutTomer.jsx
+++ components/aboutTomer.jsx
@@ -8,23 +8,22 @@ function AboutTomer() {
         <Image
           src={stillImages.at(9)}
           alt="/"
-          layout="fill"
-          objectFit="cover"
-          objectPosition="center"
-          className=" absolute left-0 top-0 -z-10 brightness-50"
+          width={1920}
+          height={1080}
+          className=" absolute left-0 top-0 -z-10 h-full object-cover object-center brightness-50"
         />
         <div
           className="container flex max-w-5xl items-center justify-center p-10"
           style={{ direction: "rtl" }}
         >
           <div className="flex flex-col items-start justify-center gap-2 text-white">
-            <h2 className="drop-shadow-stroke text-3xl font-bold">
+            <h2 className="text-3xl font-bold drop-shadow-stroke">
               קצת עליי...
             </h2>
-            <h1 className="drop-shadow-stroke text-4xl font-semibold text-main">
+            <h1 className="text-4xl font-semibold text-main drop-shadow-stroke">
               Tomer Aizik
             </h1>
-            <p className="drop-shadow-stroke mt-4 text-lg">
+            <p className="mt-4 text-lg drop-shadow-stroke">
               בתחום התקשורת והמדיה אני עוסק מספר שנים, כיום גם כסטודנט. דרכי
               התחילה בזמן התיכון במגמת תקשורת והתמקצעה במהלך השנים, החל מהשירות
               הצבאי כצלם ועורך אותו סיימתי עם ארבע תעודות הצטיינות, כשברור שזה
