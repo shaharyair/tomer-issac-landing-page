24d2a65785348f483e4245ab3b8066779832ce36
Shahar Yair <shaharyair12@gmail.com>
2023-09-23 20:21:23 +0300

    heroCarousel: Change Header text

--- components/heroCarousel.jsx
+++ components/heroCarousel.jsx
@@ -36,8 +36,8 @@ export default function HeroCarousel() {
           className=" absolute left-1/2 top-[55%] flex -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center gap-10 text-center"
           style={{ direction: "rtl" }}
         >
-          <h1 className=" whitespace-nowrap text-5xl font-thin tracking-widest text-white opacity-75 drop-shadow-md lg:text-8xl">
-            TOMER AIZIK
+          <h1 className=" whitespace-nowrap text-3xl font-thin tracking-widest text-white opacity-75 drop-shadow-md lg:text-7xl">
+            TOMER CASHIYADO
           </h1>
           <h2 className="mb-14 whitespace-nowrap text-xl font-thin tracking-widest text-white opacity-75 drop-shadow-md lg:text-3xl">
             צלם, עורך ויוצר תוכן
