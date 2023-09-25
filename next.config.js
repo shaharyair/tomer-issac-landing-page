b44c5acdb6335a22ebcc89d2647afb6c035c70cd
Shahar Yair <shaharyair12@gmail.com>
2023-09-25 17:19:59 +0300

    nextConfig: Apply csp

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["drive.google.com"],
+    domains: ["drive.google.com", "accounts.google.com"],
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // Apply CSP to all routes, adjust as needed
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "frame-ancestors 'self' https://drive.google.com https://accounts.google.com",
+          },
+        ],
+      },
+    ];
   },
 };
 
